d91e2274dc6dc1e70afa56297e133a37861840d4
NoorElBahrain <noordevmail@gmail.com>
2022-08-12 21:43:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# A Simple Python Wordcloud Project
+
+This is my first python project. A single word wordcloud.
+It was my starting point for more complicated wordclouds.
+
+## How to make a this simple wordcloud:
+
+- read the comments in the python file
+- read the [wordcloud documentation](https://github.com/amueller/word_cloudS)
+
+### Things I learned
+
+- The image you wish to use as a mask has to have a background.
+  - Maybe there are ways to make an image with a transparent image work. I don't know about it yet.
+- If you want to use other languages in the worldcloud, make sure to use a font that supports it.
+
+Result:
+![wordcloud result](wc_heart.png)
---
+++ wordcloud_meung.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud, ImageColorGenerator
+
+# The text you would like to use in the wordcloud. In this example it is the simplest form - one word
+text = "믕"
+
+# opening the mask image. Find an image you would like to us and insert it here.
+# I used a 1080 by 1080 heart image. Image needs to have a background.
+heart_mask = np.array(Image.open("[Insert PNG/JPEG Image name here without the brackets]"))
+
+# word cloud appearance and generation
+wc = WordCloud(background_color="white",
+                repeat=True,
+                mask=heart_mask,
+                font_path="NotoSansKR-Medium.otf",
+                contour_width=3,
+                contour_color="black",
+                max_words=2000,
+                random_state=1)
+wc.generate(text)
+
+# create the image colors from the colors in the mask
+image_colors = ImageColorGenerator(heart_mask)
+
+# to display the wordcloud
+plt.axis("off")
+plt.imshow(wc.recolor(color_func=image_colors),  interpolation="bilinear")
+plt.show()
+
+# saveing image
+wc.to_file("wc_heart.png")
